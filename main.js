8eaac005ee8d5542fd3d004a488f6fa5b0583e1c
Kristoffer <Kristoffermglinderman@gmail.com>
2019-11-24 15:24:33 +0100

    Working clock with HTML elements

--- main.js
+++ main.js
@@ -1,23 +1,77 @@
 const clockContainer = document.getElementById('clockContainer')
-const hourContainer = document.getElementById('hourContainer')
-const minutesContainer = document.getElementById('minutesContainer')
-const secondsContainer = document.getElementById('secondsContainer')
+
+const firstHourBlocks = document.getElementsByClassName('firstHour')
+const secondHourBlocks = document.getElementsByClassName('secondHour')
+const hoursArray = [firstHourBlocks, secondHourBlocks]
+
+const firstMinuteBlocks = document.getElementsByClassName('firstMinute')
+const secondMinuteBlocks = document.getElementsByClassName('secondMinute')
+const minutesArray = [firstMinuteBlocks, secondMinuteBlocks]
+
+const firstSecondBlocks = document.getElementsByClassName('firstSecond')
+const secondSecondBlocks = document.getElementsByClassName('secondSecond')
+const secondsArray = [firstSecondBlocks, secondSecondBlocks]
+
+let currentTime
 
 window.onload = () => {
-    createClock()
+    currentTime = new Date()
+
+    setInterval(createClock, 1000)
 }
 
 function createClock() {
-    console.log("Creating clock..")
+    currentTime = new Date()
+    let hours = currentTime.getHours()
+    let minutes = currentTime.getMinutes()
+    let seconds = currentTime.getSeconds()
 
-    setHour(NUMBER_ZERO)
+    setTime(hoursArray, hours)
+    setTime(minutesArray, minutes)
+    setTime(secondsArray, seconds)
 
 }
 
-function setHour(pixelGrid) {
+function setTime(elementArray, time) {
+
+    //Check if a zero needs to be added then makes a string
+    time = time < 10 ? `0${time}` : `${time}`
+    time = time.split('')
+
+    let firstDigit = time[0]
+    let secondDigit = time[1]
+
+    let pixelCount = 0
+
+    let pixelGrid = NUMBERS[firstDigit]
     pixelGrid.forEach(arr => {
         arr.forEach(pixel => {
+            if (pixel === 1) {
+                elementArray[0][pixelCount].classList.add("showPixel")
+                elementArray[0][pixelCount].classList.remove("hidePixel")
+            }
+            else {
+                elementArray[0][pixelCount].classList.add("hidePixel")
+                elementArray[0][pixelCount].classList.remove("showPixel")
+            }
 
+            pixelCount++
+        })
+    });
+
+    pixelCount = 0
+    pixelGrid = NUMBERS[secondDigit]
+    pixelGrid.forEach(arr => {
+        arr.forEach(pixel => {
+            if (pixel === 1) {
+                elementArray[1][pixelCount].classList.add("showPixel")
+                elementArray[1][pixelCount].classList.remove("hidePixel")
+            }
+            else {
+                elementArray[1][pixelCount].classList.add("hidePixel")
+                elementArray[1][pixelCount].classList.remove("showPixel")
+            }
+            pixelCount++
         })
     });
 }
